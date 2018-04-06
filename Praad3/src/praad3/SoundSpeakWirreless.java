070b1789a51411ddba076fd422b25624f9c1ee49
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-06 23:33:17 +0700

    Getter and Setter

--- Praad3/src/praad3/SoundSpeakWirreless.java
+++ Praad3/src/praad3/SoundSpeakWirreless.java
@@ -1,5 +1,18 @@
 package praad3;
 
 public class SoundSpeakWirreless extends SoundSpeakad {
+    private String TypeSoundspeak;
+    
+    void throttle(){
+        System.out.println("Dummm.......Tetttttt......");
+    }
+
+    public String getTypeSoundspeak() {
+        return TypeSoundspeak;
+    }
+
+    public void setTypeSoundspeak(String TypeSoundspeak) {
+        this.TypeSoundspeak = TypeSoundspeak;
+    }
     
 }
