fcb5ecede167065cce720c5b2d9a2829206e18f6
AjiePerdanaP <32450431+AjiePerdanaP@users.noreply.github.com>
2018-04-06 22:35:27 +0700

    Create Method listenSound()

---
+++ Praad3/src/praad3/SoundSpeakad.java
@@ -0,0 +1,13 @@
+package praad3;
+
+public class SoundSpeakad {
+    String merk;
+    String color;
+    Double harga;
+    
+    void listenSound(){
+        System.out.println("Merk \t= "+merk+"\n"+
+                "Color \t= "+color+"\n"+
+                "Harga \t= "+harga);
+    }
+}
